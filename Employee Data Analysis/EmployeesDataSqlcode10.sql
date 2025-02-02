ed908b96d2e93c60d5de803c1af919299e3f6865
Shwetaa99 <168013610+Shwetaa99@users.noreply.github.com>
2025-02-02 23:17:55 +0530

    Delete Employee Data Analysis directory

--- Employee Data Analysis/EmployeesDataSqlcode10.sql
+++
@@ -1,6 +0,0 @@
---10  Write a query to get the house details of the house having highest occupancy
-
-
-select top 1 house_type, bhk_type, bed_count,	furnishing_type, beds_vacant, house_id, (bed_count - beds_vacant) as bed_occupancy
-from Houses
-order by bed_occupancy desc
